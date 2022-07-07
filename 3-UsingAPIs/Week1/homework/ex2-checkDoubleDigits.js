652cc96a310b14274761584ce5114d9729ea544b
Ghufran <ghufranthabit@gmail.com>
2022-07-07 23:28:33 +0200

    added (new error)

--- 3-UsingAPIs/Week1/homework/ex2-checkDoubleDigits.js
+++ 3-UsingAPIs/Week1/homework/ex2-checkDoubleDigits.js
@@ -17,7 +17,7 @@ function checkDoubleDigits(number) {
     if (number >= 10 && number <= 99) {
       resolve('This is a double digit number!');
     } else {
-      reject(Error(`Expected a double digit number but got ${number}`));
+      reject(new Error(`Expected a double digit number but got ${number}`));
     }
   });
 }
