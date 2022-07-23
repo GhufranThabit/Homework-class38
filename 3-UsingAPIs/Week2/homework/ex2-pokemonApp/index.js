e810ca3f0a653b6addfa3089e74ff1396116c709
Ghufran <ghufranthabit@gmail.com>
2022-07-23 11:12:51 +0200

    modified ex2

--- 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
+++ 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
@@ -1,29 +1,29 @@
-'use strict';
+// 'use strict';
 
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-UsingAPIs/Week2/README.md#exercise-2-gotta-catch-em-all
+// /*------------------------------------------------------------------------------
+// Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-UsingAPIs/Week2/README.md#exercise-2-gotta-catch-em-all
 
-Complete the four functions provided in the starter `index.js` file:
+// Complete the four functions provided in the starter `index.js` file:
 
-`fetchData`: In the `fetchData` function, make use of `fetch` and its Promise 
-  syntax in order to get the data from the public API. Errors (HTTP or network 
-  errors) should be logged to the console.
+// `fetchData`: In the `fetchData` function, make use of `fetch` and its Promise
+//   syntax in order to get the data from the public API. Errors (HTTP or network
+//   errors) should be logged to the console.
 
-`fetchAndPopulatePokemons`: Use `fetchData()` to load the pokemon data from the 
-  public API and populate the `<select>` element in the DOM.
-  
-`fetchImage`: Use `fetchData()` to fetch the selected image and update the 
-  `<img>` element in the DOM.
+// `fetchAndPopulatePokemons`: Use `fetchData()` to load the pokemon data from the
+//   public API and populate the `<select>` element in the DOM.
 
-`main`: The `main` function orchestrates the other functions. The `main` 
-  function should be executed when the window has finished loading.
+// `fetchImage`: Use `fetchData()` to fetch the selected image and update the
+//   `<img>` element in the DOM.
 
-Use async/await and try/catch to handle promises.
+// `main`: The `main` function orchestrates the other functions. The `main`
+//   function should be executed when the window has finished loading.
 
-Try and avoid using global variables. As much as possible, try and use function 
-parameters and return values to pass data back and forth.
-------------------------------------------------------------------------------*/
-const url = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
+// Use async/await and try/catch to handle promises.
+
+// Try and avoid using global variables. As much as possible, try and use function
+// parameters and return values to pass data back and forth.
+// ------------------------------------------------------------------------------*/
+// const url = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
 
 async function fetchData(url) {
   const response = await fetch(url);
@@ -47,48 +47,46 @@ function fetchAndPopulatePokemons() {
 
   button.addEventListener('click', async () => {
     const urlApi = 'https://pokeapi.co/api/v2/pokemon?limit=151';
-    const data = await fetchData(urlApi);
-    const results = data.results;
-    results.forEach((result) => {
-      const option = document.createElement('option');
-      option.textContent = result.name;
-      select.appendChild(option);
-    });
+    try {
+      const data = await fetchData(urlApi);
+      const results = data.results;
 
-    select.addEventListener('change', fetchImage);
+      results.forEach((result) => {
+        const option = document.createElement('option');
+        option.value = results.indexOf(result) + 1;
+        option.textContent = result.name;
+        select.appendChild(option);
+      });
+
+      select.addEventListener('change', fetchImage);
+    } catch (error) {
+      console.log(error);
+    }
   });
 }
 
-async function fetchImage(e) {
+async function fetchImage(value) {
   const changeImage = document.querySelector('.current-poke');
   if (changeImage) {
     changeImage.remove();
   }
 
-  const pokeUrl = `https://pokeapi.co/api/v2/pokemon/${e.currentTarget.value}`;
-  const imgData = await fetchData(pokeUrl);
-  console.log(imgData);
-
-  const img = document.createElement('img');
-  img.className = 'current-poke';
-  img.src = imgData.sprites.front_shiny;
-  img.alt = 'current-poke-img';
-  document.body.appendChild(img);
-}
-
-function renderError(error) {
-  const errorElement = document.createElement('h1');
-  errorElement.textContent = error;
-  document.body.appendChild(errorElement);
+  const pokeUrl = `https://pokeapi.co/api/v2/pokemon/${value.currentTarget.value}`;
+  try {
+    const imgData = await fetchData(pokeUrl);
+    console.log(imgData);
+    const img = document.createElement('img');
+    img.className = 'current-poke';
+    img.src = imgData.sprites.front_shiny;
+    img.alt = 'current-poke-img';
+    document.body.appendChild(img);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function main() {
-  try {
-    const result = await fetchData(url);
-    fetchAndPopulatePokemons(result);
-  } catch (error) {
-    renderError(error);
-  }
+  fetchAndPopulatePokemons();
 }
 
 window.addEventListener('load', main);
